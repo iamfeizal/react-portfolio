4f9cd1c58de93cdb621d215fd455da0d5f25e537
iamfeizal <imamagusfaisal120@gmail.com>
2025-02-13 23:05:01 +0700

    add simple-typewriter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import { HERO } from "../constants"
 import { motion } from "motion/react"
 import { containerLeft, containerRight } from "../constants/animations"
+import { useTypewriter, Cursor } from "react-simple-typewriter"
 
 const Hero = () =>  {
+    const [text] = useTypewriter({
+        words: ["Machine Learning Engineer", "Data Scientist", "Artificial Intelligence Engineer"],
+        loop: 0,
+        typeSpeed: 50,
+        deleteSpeed: 50,
+        delaySpeed: 2000
+    });
+    
   return (
     <div className="border-b border-y-neutral-900 pb-4 lg:mb-35">
         <div className="flex flex-wrap">
@@ -22,7 +31,7 @@ const Hero = () =>  {
                     initial="hidden"
                     transition="transition"
                     className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                        Machine Learning Engineer
+                        [ {text}<span className="text-cyan-00"><Cursor cursorStyle='|'/></span> ]
                     </motion.span>
                     <motion.p
                     variants={containerLeft(1)}
