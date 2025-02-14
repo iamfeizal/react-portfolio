c9a8dc9af367b2a3b9c7c10471f6d6e5cb91389c
iamfeizal <imamagusfaisal120@gmail.com>
2025-02-14 13:05:59 +0700

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import logo from "../../public/logo.svg";
 import { FaGithub, FaInstagram, FaLinkedin, FaKaggle } from "react-icons/fa";
 
 const Navbar = () =>  {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center ">
-            <img className="mx-2 w-10" src="/public/logo.svg" alt="logo"/></div>
+            <img className="mx-2 w-10" src={logo} alt="logo"/></div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
           <a href="https://github.com/iamfeizal">
             <FaGithub className="text-white" />
